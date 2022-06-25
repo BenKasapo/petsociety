5dfdb86d0b24549100aaed12038292cedb8d5cfc
BenKasapo <benedykasapo@gmail.com>
2022-06-25 15:27:25 +0300

    deployement

--- server.js
+++ server.js
@@ -36,8 +36,8 @@ app.get("/", (req, res) => {
   res.send("APP IS RUNNING...");
 });
 
-const Port = process.env.Port || 5000;
+const port = process.env.Port || 5000;
 
-app.listen(Port, "0.0.0.0", () =>
-  console.log(`Server started on port ${Port} `)
+app.listen(port, "0.0.0.0", () =>
+  console.log(`Server started on port ${port} `)
 );
